debfb9c3e0ceb53c05a59fb6a44d7a4c87a8e313
unknown <waptop4u@gmail.com>
2021-05-25 23:15:03 +0700

    1.1 Login form

--- client/src/components/container/register_login/index.js
+++ client/src/components/container/register_login/index.js
@@ -17,27 +17,37 @@ const RegisterLogin = () => {
 
   return (
     <div className="register-login">
-      <h1 className="register-login__title">login</h1>
+      <div className="register-login__title">
+        <h1>login</h1>
+      </div>
       <form className="register-login__form" onSubmit={submitForm}>
         <div className="register-login__row">
           <input
-            type="email"
+            type="text"
             name="email"
             className="register-login__input"
-            placeholder="Email"
+            id="register-login__input--email"
             value={user.email}
+            required
             onChange={onChangeInput}
           />
+          <label htmlFor="register-login__input--email" className="register-login__label">
+            Email
+          </label>
         </div>
         <div className="register-login__row">
           <input
             type="password"
             name="password"
             className="register-login__input"
-            placeholder="Password"
+            id="register-login__input--password"
             value={user.password}
+            required
             onChange={onChangeInput}
           />
+          <label htmlFor="register-login__input--password" className="register-login__label">
+            Password
+          </label>
         </div>
         <div className="register-login__row">
           <button className="register-login__btn" onSubmit={submitForm}>
